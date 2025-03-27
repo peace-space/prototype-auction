9c89eec9a1d1f17d8d03a6b47eaad5158f704a63
PeaceSpace <peace.space.ps@gmail.com>
2025-03-27 23:49:47 +0700

    23:10-27/03/2568

--- api-prototype-your-auction-service/config/filesystems.php
+++ api-prototype-your-auction-service/config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('public/storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('/'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
