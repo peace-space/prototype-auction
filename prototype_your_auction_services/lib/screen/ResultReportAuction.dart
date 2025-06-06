7ef7dff0588e04ea3898aa4fd90b7182fdf2bc96
PeaceSpace <peace.space.ps@gmail.com>
2025-06-06 15:01:55 +0700

    14:53-06/06/2568

--- prototype_your_auction_services/lib/screen/ResultReportAuction.dart
+++ prototype_your_auction_services/lib/screen/ResultReportAuction.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:prototype_your_auction_services/screen/AppBar.dart';
 
 class ReportAuction extends StatefulWidget {
@@ -18,14 +21,39 @@ class ReportAuctionState extends State<ReportAuction> {
 
   Widget display() {
     return StreamBuilder(
-      stream: null,
+      stream: fetchResultReportAuction(),
       builder:
           (context, snapshot) => ListView.builder(
             padding: EdgeInsets.all(8),
-            itemCount: 5,
-            itemBuilder:
-                (context, index) => Card(child: ListTile(title: Text('test'))),
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> data = snapshot.data![index];
+                return Card(
+                    child: ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(data['id_users'].toString())
+                          ],
+                        )
+                    )
+                );
+              }
           ),
     );
+  }
+
+  Stream<dynamic> fetchResultReportAuction() async* {
+    String url = 'http://192.168.1.248/001.Work/003.Project-2567/Prototype-Your-Auction-Services/api-prototype-your-auction-service/public/api/v1/result-report-auction';
+    final uri = Uri.parse(url);
+    final responce = await http.get(uri);
+
+    if (responce.statusCode == 200) {
+      final body = jsonDecode(responce.body);
+      print(body['data'].toString());
+      yield body['data'];
+
+      // setState(() {});
+    }
   }
 }
